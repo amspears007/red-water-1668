edf8e99bede10b58161b581d5180205516b3a86c
Amy Spears <amspears007@gmail.com>
2023-04-16 13:14:59 -0600

    Add feature tests to dish spec

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -11,11 +11,11 @@ RSpec.describe "dishes show page", type: :feature do
 
   let!(:chef_amy) {Chef.create!(name:'Amy')}
 
-  let!(:spaghetti) {chef_amy.dishs.create!(name:'spaghetti', description: 'pasta with marinara and chicken ')}
-  let!(:mac_n_cheese) {chef_amy.dishs.create!(name:'macaroni and cheese', description: 'pasta with cheese')}
-  let!(:chicken_fingers) {chef_amy.dishs.create!(name:'chicken_fingers', description: 'chicken with fried bread')}
+  let!(:spaghetti) {chef_amy.dishes.create!(name:'spaghetti', description: 'pasta with marinara and chicken')}
+  let!(:mac_n_cheese) {chef_amy.dishes.create!(name:'macaroni and cheese', description: 'pasta with cheese')}
+  let!(:chicken_fingers) {chef_amy.dishes.create!(name:'chicken_fingers', description: 'chicken with fried bread')}
  
-  let!(:cheese) {Ingredient.create!(name:'chees', calories: 200)}
+  let!(:cheese) {Ingredient.create!(name:'cheese', calories: 200)}
   let!(:pasta) {Ingredient.create!(name:'pasta', calories: 150)}
   let!(:marinara) {Ingredient.create!(name:'marinara', calories: 100)}
   let!(:chicken) {Ingredient.create!(name:'chicken', calories: 125)}
@@ -26,3 +26,16 @@ RSpec.describe "dishes show page", type: :feature do
   let!(:spaghetti_ingredients) {DishIngredient.create!(dish_id: spaghetti.id, ingredient_id: marinara.id)}
   let!(:chicken_fingers_ingredients) {DishIngredient.create!(dish_id: chicken_fingers.id, ingredient_id: chicken.id)}
   let!(:chicken_fingers_ingredients) {DishIngredient.create!(dish_id: chicken_fingers.id, ingredient_id: bread_crumbs.id)}
+
+  describe "User Story 1 When I visit a dish's show page" do
+    it 'I see the dish’s name and description and I see a list of ingredients for that dish' do
+      visit "dishes/#{spaghetti.id}"
+
+      within("#dish-info") do
+      expect(page).to have_content('Name: Spaghetti')
+      expect(page).to have_content('pasta with marinara and chicken')
+      expect(page).to have_content('Ingredient List: pasta, marinara, chicken')
+      end
+    end
+  end
+end
